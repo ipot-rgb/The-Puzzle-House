2247c7d10415edd0aba2bd4cb4811de4d171cdb5
Hong Meng <ipot0812@gmail.com>
2026-04-08 15:45:03 +0800

    commited

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ pygame.init()
 # Load images
 wall = pygame.image.load("wall.png")
 data = open("puzzle_00.png")
+data2 = open("puzzle_01.png")
+data3 = open("puzzle_02.png")
+data4 = open("puzzle_03.png")
 puzzle = pygame.image.load(data, '.png')
+puzzle2 = pygame.image.load(data2, '.png')
+puzzle3 = pygame.image.load(data3, '.png')
+puzzle4 = pygame.image.load(data4, '.png')
 
 # Set up the display
 display = pygame.display.set_mode((1200, 700), pygame.SCALED) 
@@ -18,10 +24,10 @@ font = pygame.font.Font('C:\\Users\\HP\\OneDrive\\Documents\\PythonGame\\OpenSan
 text = font.render("Hello, World!", True, (0,0,0))
 display.blit(text, (400, 300))
 
-# Color 
-color = Color(255, 0, 0)  # Red color
-color2 = Color(f"#5F5F5F")  # Gray color
-pygame.draw.rect(display, color, (250,0,0))
+# # Color 
+# color = Color(255, 0, 0)  # Red color
+# color2 = Color(f"#5F5F5F")  # Gray color
+# pygame.draw.rect(display, color, (250,0,0))
 
 # Update the display
 pygame.display.flip()
