938a6efc5a5a955434cb4546ba4ae339f126e81c
YONG JIA HOOI <yongjh0711@gmail.com>
2026-04-13 14:52:29 +0800

    finally added level 1

---
+++ level 1.py
@@ -0,0 +1,44 @@
+import pygame
+import os
+
+pygame.init()
+screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+pygame.display.set_caption("Level 1")
+clock = pygame.time.Clock()
+info = pygame.display.Info()
+WIDTH, HEIGHT = info.current_w, info.current_h
+
+# background
+background_img = pygame.image.load(os.path.join("materials", "lv1 background.png")).convert()
+background_img = pygame.transform.scale(background_img, (WIDTH, HEIGHT))
+
+# sprite class for the note
+class Note(pygame.sprite.Sprite):
+    def __init__(self, image_path, width, height, x, y):
+        super().__init__()
+        self.image = pygame.image.load(image_path).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (width, height))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+
+note_width = int(WIDTH * 0.75)  
+note_height = int(HEIGHT * 1.1)
+note = Note(os.path.join("materials", "lv1 note.png"), note_width, note_height, 0, (HEIGHT - note_height)//2)
+
+all_sprites = pygame.sprite.Group()
+all_sprites.add(note)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.blit(background_img, (0, 0))
+
+    all_sprites.draw(screen)
+
+    pygame.display.update()
+    clock.tick(60)
+
+pygame.quit()
