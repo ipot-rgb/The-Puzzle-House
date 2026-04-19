ee9623b5b6c47b6b89a1f031c1b0c7c3d966af17
YONG JIA HOOI <yongjh0711@gmail.com>
2026-04-19 20:16:12 +0800

    commited

--- level 1.py
+++ level 1.py
@@ -21,18 +21,60 @@ class Note(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
 
+
+
+class Bookmark(pygame.sprite.Sprite):
+    def __init__(self, image_path, width, height, x, y):
+        super().__init__()
+        self.image = pygame.image.load(image_path).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (width, height))
+        self.rect = self.image.get_rect(topleft=(x, y))
+
+        self.is_dragging = False
+        self.offset_x = 0
+        self.offset_y = 0
+
+    def update(self, events):
+        for event in events:
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                if self.rect.collidepoint(event.pos):
+                    self.is_dragging = True
+                    self.offset_x = self.rect.x - event.pos[0]
+                    self.offset_y = self.rect.y - event.pos[1]
+
+            elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                self.is_dragging = False
+
+        if self.is_dragging:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            self.rect.x = mouse_x + self.offset_x
+            self.rect.y = mouse_y + self.offset_y
+
+
 note_width = int(WIDTH * 0.65)  
-note_height = int(HEIGHT * 1.1)
-note = Note(os.path.join("materials", "lv1 note.png"), note_width, note_height, 0, (HEIGHT - note_height)//2)
+note_height = int(HEIGHT * 0.9)
+note_x = 0
+note_y = (HEIGHT - note_height) // 2
+note = Note(os.path.join("materials", "lv1 note.png"), note_width, note_height, note_x, note_y)
+
+bm_width = int(note_width * 0.9)
+bm_height = int(note_height * 1.0)
+bm = Bookmark(os.path.join("materials", "book mark.png"), 
+              bm_width, bm_height, 
+              note_x + int(note_width * 0.6), note_y + 50)
 
 all_sprites = pygame.sprite.Group()
 all_sprites.add(note)
+all_sprites.add(bm)
 
 running = True
 while running:
-    for event in pygame.event.get():
+    events = pygame.event.get()
+    for event in events:
         if event.type == pygame.QUIT:
             running = False
+
+    bm.update(events)
 
     screen.blit(background_img, (0, 0))
 
