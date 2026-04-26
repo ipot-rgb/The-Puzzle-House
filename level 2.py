ff25d6758cba6112f93f172e5c87b4013b408bec
YONG JIA HOOI <yongjh0711@gmail.com>
2026-04-26 13:47:14 +0800

    commited

---
+++ level 2.py
@@ -0,0 +1,24 @@
+import pygame
+import os
+
+pygame.init()
+screen = pygame.display.set_mode((1200,650), pygame.SCALED)
+clock = pygame.time.Clock()
+info = pygame.display.Info()
+WIDTH, HEIGHT = info.current_w, info.current_h
+
+background_img = pygame.image.load(os.path.join("materials", "lv2 background.png")).convert()
+background_img = pygame.transform.scale(background_img, (WIDTH, HEIGHT))
+
+
+running = True
+while running:
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.blit(background_img, (0, 0))
+    pygame.display.update()
+
+pygame.quit()
