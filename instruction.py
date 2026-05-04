d7ab2fde0924baf70c342a43eff1e64f63c6354e
Shawn Ko Phor Shyuan <koshawnps@gmail.com>
2026-05-04 17:17:23 +0800

    Adding lines of instruction page

---
+++ instruction.py
@@ -0,0 +1,18 @@
+import pygame
+import time
+
+def show_instruction(screen, font):
+    #lines showed in instruction page
+    lines = [
+        "Welcome to The Puzzle House!",
+        "",
+        "In each level, you must find the correct passcode.",
+        "Click on the letter buttons to enter your answer.",
+        "Press 'ENTER' button to submit.",
+        "",
+        "This is a test for your creativity",
+        "and also your intelligence",
+        "Finish the levels to prove your worth!!"
+        "",
+        "Good luck!",
+    ]
